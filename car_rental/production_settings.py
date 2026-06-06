27ed531f169e474849ee7c5a6794c30439ccf7f0
Edufel45 <kipngetichfelix281@gmail.com>
2026-06-06 22:29:30 +0300

    Prepare for deployment

---
+++ car_rental/production_settings.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'www.yourusername.pythonanywhere.com']
+
+# Static files collection
+STATIC_ROOT = BASE_DIR / 'staticfiles'
